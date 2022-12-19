51e9e655065fc7ded10aa083cc5e8d2534477490
deshmukhritesh18 <99178372+deshmukhritesh18@users.noreply.github.com>
2022-12-19 18:28:34 +0530

    Add files via upload

---
+++ Pattern7.java
@@ -0,0 +1,30 @@
+package com.bnt.logical;
+
+import java.util.Scanner;
+
+public class Pattern7 {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter The no of rows = ");
+		int rows =sc.nextInt();
+		for (int i = 0; i <= rows - 1; i++) {
+			for (int j = 0; j <= i; j++) {
+				System.out.print(" ");
+			}
+			for (int k = 0; k <= rows - 1 - i; k++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+	}
+
+}
+
+/* * * * * * * * * 
+    * * * * * * * 
+     * * * * * * 
+      * * * * * 
+       * * * * 
+        * * * 
+         * * 
+          *   */
