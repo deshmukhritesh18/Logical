f06130b4baaf4209826962bf02f44264d347bdd5
deshmukhritesh18 <99178372+deshmukhritesh18@users.noreply.github.com>
2023-01-04 17:49:56 +0530

    Update Pattern16.java

--- Pattern16.java
+++ Pattern16.java
@@ -15,3 +15,12 @@ public class Pattern16 {
 		}
 	}
 }
+/*      A 
+       A B 
+      A B C 
+     A B C D 
+    A B C D E 
+   A B C D E F 
+  A B C D E F G 
+ A B C D E F G H 
+A B C D E F G H I */ 
