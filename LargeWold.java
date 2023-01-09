111b165618a5ee386d3036b14f9248243fe0c213
deshmukhritesh18 <99178372+deshmukhritesh18@users.noreply.github.com>
2023-01-09 18:19:51 +0530

    Add files via upload

---
+++ LargeWold.java
@@ -0,0 +1,38 @@
+package com.bnt.logical;
+
+import java.util.Scanner;
+
+public class LargeWold {
+
+	public static void main(String[] args) {
+		System.out.println("Enter sentence");
+		Scanner sc = new Scanner(System.in);
+		String string = sc.nextLine();
+		String word = "", small = "", large = "";
+		String[] words = new String[100];
+		int length = 0;
+
+		string = string + " ";
+
+		for (int i = 0; i < string.length(); i++) {
+			if (string.charAt(i) != ' ') {
+				word = word + string.charAt(i);
+			} else {
+				words[length] = word;
+				length++;
+				word = "";
+			}
+		}
+
+		small = large = words[0];
+
+		for (int k = 0; k < length; k++) {
+			// if (small.length() > words[k].length())
+			// small = words[k];
+			if (large.length() < words[k].length())
+				large = words[k];
+		}
+		// System.out.println("Smallest word: " + small);
+		System.out.println("Largest word: " + large);
+	}
+}
