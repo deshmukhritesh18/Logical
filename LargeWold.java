d480d6852d3c1895a2624240ab6fe7dcb63a0415
deshmukhritesh18 <99178372+deshmukhritesh18@users.noreply.github.com>
2023-01-12 18:06:53 +0530

    Update LargeWold.java

--- LargeWold.java
+++ LargeWold.java
@@ -34,5 +34,33 @@ public class LargeWold {
 		}
 		// System.out.println("Smallest word: " + small);
 		System.out.println("Largest word: " + large);
+		
+		
+		//by one for loop
+	/*	Scanner sc = new Scanner(System.in);
+		System.out.println("Enter sentence:");
+		String string=sc.nextLine();
+
+		string =string + " "; 
+		String word = "",large = "";
+
+		for (int i=0;i<string.length();i++) {
+			char c = string.charAt(i);
+			if (c == ' ') {
+
+				if (word.length()>large.length())
+					large=word;
+
+				word="";
+			} else{
+				word=word+c;
+			}
+		}
+
+		System.out.println("Largest word : " + large);
+		
+		*/
+		
+		
 	}
 }
