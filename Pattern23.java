a677a070925fbb09cad481c0713c5e081b914b53
deshmukhritesh18 <99178372+deshmukhritesh18@users.noreply.github.com>
2023-01-20 18:47:30 +0530

    Add files via upload

---
+++ Pattern23.java
@@ -0,0 +1,49 @@
+package com.bnt.logical;
+
+import java.util.Scanner;
+
+public class Pattern23 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int row, i, j, space = 1;
+		System.out.print("Enter number of rows ");
+
+		row = sc.nextInt();
+		space = row - 1;
+		for (j = 1; j <= row; j++) {
+			for (i = 1; i <= space; i++) {
+				System.out.print(" ");
+			}
+			space--;
+			for (i = 1; i <= 2 * j - 1; i++) {
+				System.out.print("*");
+			}
+			System.out.println("");
+		}
+		space = 1;
+		for (j = 1; j <= row - 1; j++) {
+			for (i = 1; i <= space; i++) {
+				System.out.print(" ");
+			}
+			space++;
+			for (i = 1; i <= 2 * (row - j) - 1; i++) {
+				System.out.print("*");
+			}
+			System.out.println("");
+		}
+	}
+
+}
+/*
+    *
+   ***
+  *****
+ *******
+*********
+ *******
+  *****
+   ***
+    *
+*/
